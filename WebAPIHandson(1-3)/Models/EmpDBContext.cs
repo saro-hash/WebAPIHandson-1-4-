2760fe08c92e590df4bb3f81749bfef890a9300a
Abhishek <abhi0699@gmail.com>
2020-10-13 10:16:48 +0530

    DBContext added

---
+++ WebAPIHandson(1-3)/Models/EmpDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIHandson_1_3_.Models
+{
+    public class EmpDBContext : DbContext
+    { 
+        public EmpDBContext(DbContextOptions<EmpDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Emp> Employees { get; set; }
+    }
+}
